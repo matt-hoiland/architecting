519918cf27c60ba7e4d1d1e8064d3402ac2123bd
Matt Hoiland <matthew.proctor.hoiland@gmail.com>
2021-08-14 14:26:29 -0600

    Create mongo launch script

--- README.md
+++ README.md
@@ -1,11 +1,44 @@
 # architecting
 Just a space to explore different microservice tools in go
 
+## Launching mongo
+
+A real hack and slash script to spin up the database, probably should use something like `docker-compose`
+
+TODO: Use `docker-compose`
+
+
+### Usage
+
+```bash
+$ source launch-mongo.sh
+```
+
+This will:
+* Export environment variables
+* Make a docker network `architecting`
+* Make a docker volume `architectingdbdata`
+* Pull the docker image `mongo:5.0.2`
+* Create a long lived container `architectingdb`
+* Start container `architectingdb` if not already running
+* Create an alias `dbconn` to exec into `architectingdb` using `mongosh`
+
+
+### Environmet Vars Exported
+
+```env
+MONGO_CONTAINER_NAME=architectingdb
+MONGO_DATA_DIR=$(docker volume inspect --format '{{ .Mountpoint }}' ${MONGO_DATA_VOLUME})
+MONGO_DATA_VOLUME=architectingdbdata
+MONGO_VERSION=5.0.2
+PROJECT_NETWORK=architecting
+```
+
 ## 2021-08-14
 
 I want to explore the following:
 
-* [ ] mongodb containerization with docker
+* [x] mongodb containerization with docker
 * [ ] mongodb go driver
 * [ ] liveness probe exposure from program (kube prep)
 * [ ] 1:1 mapping between live service and mongo collection
---
+++ launch-mongo.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+set -e # Halt on errors
+
+export MONGO_DATA_VOLUME=architectingdbdata
+export MONGO_VERSION=5.0.2
+export MONGO_CONTAINER_NAME=architectingdb
+export PROJECT_NETWORK=architecting
+
+# ensure docker volume
+
+function ensureProjectNetworkCreated() {
+    if ! docker network inspect ${PROJECT_NETWORK} &> /dev/null; then
+        echo "${PROJECT_NETWORK} does not exist, creating ..."
+        docker network create ${PROJECT_NETWORK} &> /dev/null
+        echo "${PROJECT_NETWORK} created!"
+    fi
+}
+
+function ensureMongoVolumeCreated() {
+    if ! docker volume inspect ${MONGO_DATA_VOLUME} &> /dev/null; then
+        echo "${MONGO_DATA_VOLUME} does not exist, creating ..."
+        docker volume create ${MONGO_DATA_VOLUME} &> /dev/null
+        echo "${MONGO_DATA_VOLUME} created!"
+    fi
+}
+
+function ensureMongoImagePulled() {
+    if ! docker image inspect mongo:${MONGO_VERSION} &> /dev/null; then
+        echo "Pulling mongo:${MONGO_VERSION} ..."
+        docker pull mongo:${MONGO_VERSION}
+        echo "Pull complete!"
+    fi
+}
+
+function ensureMongoContainerCreated() {
+    if ! docker container inspect ${MONGO_CONTAINER_NAME} &> /dev/null; then
+        echo "Created container ${MONGO_CONTAINER_NAME} ..."
+        docker container create \
+            --volume ${MONGO_DATA_VOLUME}:/data/db \
+            --name ${MONGO_CONTAINER_NAME} \
+            --publish 27117:27017 \
+            --network ${PROJECT_NETWORK} \
+            mongo:$MONGO_VERSION \
+            &> /dev/null
+        echo "${MONGO_CONTAINER_NAME} created!" 
+    fi
+}
+
+function ensureMongoContainerRunning() {
+    if [ $(docker container inspect --format '{{ .State.Running }}' ${MONGO_CONTAINER_NAME}) != "true" ]; then
+        docker start ${MONGO_CONTAINER_NAME} &> /dev/null
+        echo "${MONGO_CONTAINER_NAME} started!"
+    else
+        echo "${MONGO_CONTAINER_NAME} already running!"
+    fi
+}
+
+ensureProjectNetworkCreated
+ensureMongoVolumeCreated
+export MONGO_DATA_DIR=$(docker volume inspect --format '{{ .Mountpoint }}' ${MONGO_DATA_VOLUME})
+ensureMongoImagePulled
+ensureMongoContainerCreated
+ensureMongoContainerRunning
+
+alias dbconn="docker exec -it ${MONGO_CONTAINER_NAME} mongosh"
+echo "Run '\$ dbconn' to connect to mongo"
