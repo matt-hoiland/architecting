a980f2ef7ef7f5c103dd5fc9cfb0011be924cdcb
Matt Hoiland <matthew.proctor.hoiland@gmail.com>
2021-08-14 23:00:39 -0600

    Implement insertion

--- internal/api/authn/create.go
+++ internal/api/authn/create.go
@@ -1,7 +1,22 @@
 package authn
 
-import "github.com/matt-hoiland/architecting/data"
+import (
+	"context"
+	"errors"
 
-func (api *AuthNAPI) InsertCredentials(creds *data.UserCredentials) (*data.UserCredentials, error) {
-	return nil, nil
+	"github.com/matt-hoiland/architecting/data"
+	log "github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func (api *AuthNAPI) InsertCredentials(creds *data.UserCredentials) (primitive.ObjectID, error) {
+	result, err := api.collection.InsertOne(context.TODO(), creds)
+	if err != nil {
+		log.Error(err)
+		return primitive.NilObjectID, err
+	}
+	if id, ok := result.InsertedID.(primitive.ObjectID); ok {
+		return id, nil
+	}
+	return primitive.NilObjectID, errors.New("unrecognized type returned")
 }
