004498c8523d31a1b87755ca792e4a069c499141
Matt Hoiland <matthew.proctor.hoiland@gmail.com>
2021-08-14 21:58:50 -0600

    Add int flag utility func

--- lib/flag/flag.go
+++ lib/flag/flag.go
@@ -2,7 +2,11 @@ package flag
 
 import (
 	"flag"
+	"fmt"
 	"os"
+	"strconv"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // Parse calls stdlib's flag.Parse function.
@@ -10,9 +14,18 @@ func Parse() {
 	flag.Parse()
 }
 
-// String setups a command line flag/environment variable combination.
+// String sets up a command line flag/environment variable combination.
 func String(name, envKey, defaultValue, usage string) *string {
-	return flag.String(name, LookupEnvOrString(envKey, defaultValue), usage)
+	return flag.String(name, LookupEnvOrString(envKey, defaultValue), appendDefault(usage, defaultValue))
+}
+
+// Int sets up a command line flag/environment variable combination.
+func Int(name, envKey string, defaultValue int, usage string) *int {
+	return flag.Int(name, LookupEnvOrInt(envKey, defaultValue), appendDefault(usage, defaultValue))
+}
+
+func appendDefault(usage string, defaultValue interface{}) string {
+	return fmt.Sprintf("%s [default: %v]", usage, defaultValue)
 }
 
 // LookupEnvOrString checks the environment for the value of the given key or
@@ -22,4 +35,23 @@ func LookupEnvOrString(envKey, defaultValue string) string {
 		return val
 	}
 	return defaultValue
+}
+
+// LookupEnvOrInt checks the environment for the value of the given key and
+// parses it to int or passes through the default value provided.
+func LookupEnvOrInt(envKey string, defaultValue int) int {
+	val, ok := os.LookupEnv(envKey)
+	if !ok {
+		return defaultValue
+	}
+	i, err := strconv.Atoi(val)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"key":     envKey,
+			"val":     val,
+			"default": defaultValue,
+		}).Warn("Non-int value given for an int envar")
+		return defaultValue
+	}
+	return i
 }
