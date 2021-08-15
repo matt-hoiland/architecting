7475becca38837f98fabb147555f48208b9674ed
Matt Hoiland <matthew.proctor.hoiland@gmail.com>
2021-08-14 23:32:59 -0600

    Implement read functions

--- internal/api/authn/read.go
+++ internal/api/authn/read.go
@@ -1,14 +1,29 @@
 package authn
 
 import (
+	"context"
+
 	"github.com/matt-hoiland/architecting/data"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func (api *AuthNAPI) GetCredentialsByID(id primitive.ObjectID) (*data.UserCredentials, error) {
-	return nil, nil
+	return api.GetCredentials("_id", id)
 }
 
-func (api *AuthNAPI) GetCredentialsByAccountName(accountName string) (*data.UserCredentials, error) {
-	return nil, nil
+func (api *AuthNAPI) GetCredentialsByEmail(email string) (*data.UserCredentials, error) {
+	return api.GetCredentials("email", email)
+}
+
+func (api *AuthNAPI) GetCredentials(key string, value interface{}) (*data.UserCredentials, error) {
+	filter := primitive.D{primitive.E{Key: key, Value: value}}
+	res := api.collection.FindOne(context.TODO(), filter)
+
+	var creds data.UserCredentials
+	err := res.Decode(&creds)
+	if err != nil {
+		return nil, err
+	}
+
+	return &creds, nil
 }
