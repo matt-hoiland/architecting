7a413a423e6e523af71bd4e1c80298d9d3af1d07
Matt Hoiland <matthew.proctor.hoiland@gmail.com>
2021-08-15 17:36:33 -0600

    Move Collector into its own file

--- internal/api/auth/api.go
+++ internal/api/auth/api.go
@@ -7,8 +7,6 @@ import (
 	"github.com/matt-hoiland/architecting/data"
 	log "github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const (
@@ -18,62 +16,6 @@ const (
 	// CredentialsCollection names the credentials collection
 	CredentialsCollection = "credentials"
 )
-
-// Collector represents the expected operations that can be performed on a mongodb collection.
-// The intended implementation is mongodb's official driver in go. Interfacing allows this
-// dependency to be mocked for testing.
-//
-// Annotations taken from https://github.com/mongodb/mongo-go-driver/blob/v1.7.1/mongo/collection.go
-type Collector interface {
-	// DeleteOne executes a delete command to delete at most one document from the collection.
-	//
-	// The filter parameter must be a document containing query operators and can be used to select the document to be
-	// deleted. It cannot be nil. If the filter does not match any documents, the operation will succeed and a DeleteResult
-	// with a DeletedCount of 0 will be returned. If the filter matches multiple documents, one will be selected from the
-	// matched set.
-	//
-	// The opts parameter can be used to specify options for the operation (see the options.DeleteOptions documentation).
-	//
-	// For more information about the command, see https://docs.mongodb.com/manual/reference/command/delete/.
-	DeleteOne(ctx context.Context, filter interface{}, opts ...*options.DeleteOptions) (*mongo.DeleteResult, error)
-
-	// FindOne executes a find command and returns a SingleResult for one document in the collection.
-	//
-	// The filter parameter must be a document containing query operators and can be used to select the document to be
-	// returned. It cannot be nil. If the filter does not match any documents, a SingleResult with an error set to
-	// ErrNoDocuments will be returned. If the filter matches multiple documents, one will be selected from the matched set.
-	//
-	// The opts parameter can be used to specify options for this operation (see the options.FindOneOptions documentation).
-	//
-	// For more information about the command, see https://docs.mongodb.com/manual/reference/command/find/.
-	FindOne(ctx context.Context, filter interface{}, opts ...*options.FindOneOptions) *mongo.SingleResult
-
-	// InsertOne executes an insert command to insert a single document into the collection.
-	//
-	// The document parameter must be the document to be inserted. It cannot be nil. If the document does not have an _id
-	// field when transformed into BSON, one will be added automatically to the marshalled document. The original document
-	// will not be modified. The _id can be retrieved from the InsertedID field of the returned InsertOneResult.
-	//
-	// The opts parameter can be used to specify options for the operation (see the options.InsertOneOptions documentation.)
-	//
-	// For more information about the command, see https://docs.mongodb.com/manual/reference/command/insert/.
-	InsertOne(ctx context.Context, document interface{}, opts ...*options.InsertOneOptions) (*mongo.InsertOneResult, error)
-
-	// UpdateByID executes an update command to update the document whose _id value matches the provided ID in the collection.
-	// This is equivalent to running UpdateOne(ctx, bson.D{{"_id", id}}, update, opts...).
-	//
-	// The id parameter is the _id of the document to be updated. It cannot be nil. If the ID does not match any documents,
-	// the operation will succeed and an UpdateResult with a MatchedCount of 0 will be returned.
-	//
-	// The update parameter must be a document containing update operators
-	// (https://docs.mongodb.com/manual/reference/operator/update/) and can be used to specify the modifications to be
-	// made to the selected document. It cannot be nil or empty.
-	//
-	// The opts parameter can be used to specify options for the operation (see the options.UpdateOptions documentation).
-	//
-	// For more information about the command, see https://docs.mongodb.com/manual/reference/command/update/.
-	UpdateByID(ctx context.Context, id interface{}, update interface{}, opts ...*options.UpdateOptions) (*mongo.UpdateResult, error)
-}
 
 // AuthAPI owns and performs database transactions to the credentials collection as well as
 // future authn and authz operations.
